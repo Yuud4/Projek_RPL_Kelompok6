4a0204f91413a4813d683b59a602442d2b3a92d5
ervinamaharani <ervina_2502@apps.ipb.ac.id>
2020-05-11 19:58:11 +0700

    initial commit

--- simpanregister.php
+++ simpanregister.php
@@ -1,16 +1,30 @@
 <?php
-include 'config.php'; // config belum dibuat
 
-// menyimpan data kedalam variabel
-$nim            = $_POST['username'];
-$nama           = $_POST['email'];
-$jurusan        = $_POST['password'];
-$jenis_kelamin  = $_POST['phone_number'];
-$alamat         = $_POST['address'];
+///membuat koneksi ke database
+$server="localhost"; ///nama server
+$user="root"; ///nama username mysql
+$Password=NULL;
+$database="REGROW"; ///nama database yang dipilih
 
-// query SQL untuk insert data
-$query="INSERT INTO user SET username='$username',email='$email',password='$password',phone_number='$phone_number',address='$address'";
-mysql_query($koneksi, $query);
-// mengalihkan ke halaman index.php
-header("location:index.html");
+$con=mysqli_connect($server, $user, $Password) or die ("Koneksi Gagal"); ///koneksi ke database
+mysqli_select_db($con,$database) or die ("Database tidak tersedia"); ///memilih database, dan menampilkan pesan jika gagal
+///mengambil data dari form
+$Username=$_POST['Username'];
+$Email=$_POST['Email'];
+$Password=$_POST['Password'];
+$Nomor_Telepon=$_POST['Nomor_Telepon'];
+$Alamat=$_POST['Alamat'];
+
+
+///input ke tabel pengguna
+$input=mysqli_query($con, "INSERT INTO pengguna (Username, Email, Password, Nomor_Telepon, Alamat) VALUES ('$Username', '$Email', '$Password', '$Nomor_Telepon', '$Alamat')");
+///cek sudah terinput atau belum
+if($input) ///jika sukses
+{
+header('Location: login.html?status=sukses');
+}
+else ///jika gagal
+{
+header('Location: register.html?status=gagal');
+}
 ?>
